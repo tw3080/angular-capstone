cd9113b58a867ea6e144d8ccd2e92fa70a16fa4e
Taylor Welling <tw3080@gmail.com>
2016-09-08 23:02:04 -0700

    added a 'submit' function, which is called when the locationSearchForm is submitted; this function calls the geocodeLocation function to geocode the searched address, then calls the getWeatherConditions function to get the current weather for that address

--- app/home/home.js
+++ app/home/home.js
@@ -5,7 +5,17 @@ viewsModule.config(['$routeProvider', function($routeProvider) {
     });
 }]);
 
-viewsModule.controller('CurrentWeatherCtrl', function($scope, geocodeLocation, getWeatherConditions) {
-    geocodeLocation('san francisco');
-    getWeatherConditions();
+viewsModule.controller('CurrentWeatherCtrl', function($scope, $rootScope, geocodeLocation, getWeatherConditions) {
+    // TODO: Is this the best way to do this? Using $scope doesn't work...
+    $rootScope.address = null;
+
+    $rootScope.submit = function() {
+        // TODO: Why are $scope and $rootScope interchangable here? (console log below)
+        // console.log($scope.address);
+        geocodeLocation($scope.address).then(function(response) {
+            $scope.lat = response.lat;
+            $scope.lng = response.lng;
+            getWeatherConditions($scope.lat, $scope.lng);
+        });
+    };
 });
