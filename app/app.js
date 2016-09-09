5449c16ca833995b397cdeb7a90a99f19817d6d3
Taylor Welling <tw3080@gmail.com>
2016-09-08 18:23:00 -0700

    added a redirect route in case the user tries to go to any route which isn't already defined

---
+++ app/app.js
@@ -0,0 +1,7 @@
+angular.module('weatherApp', ['weatherAppViews', 'ngRoute'])
+    .config(function($routeProvider) {
+        /* If the user tries to go to any route other than one which is already defined, redirect them to the home page */
+        $routeProvider.otherwise({
+            redirectTo: '/'
+        });
+    });
