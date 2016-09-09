3d0d77355995cf16bcaa9567fa04964ed9c2e74d
Taylor Welling <tw3080@gmail.com>
2016-09-08 18:21:46 -0700

    added AJAX calls to Google's Geocode API and Weather Underground's API (current weather conditions)

---
+++ app/components/library.js
@@ -0,0 +1,36 @@
+angular.module('weatherLibrary', [])
+/* Initialize constants */
+.constant('GOOGLE_GEOCODE_PREFIX', 'https://maps.googleapis.com/maps/api/geocode/json')
+.constant('GOOGLE_API_KEY', 'AIzaSyAjk0rA9cKg0qBwGHTTg2J3LrlFQZ9F19E')
+.constant('WUNDERGROUND_PREFIX', '//api.wunderground.com/api/')
+.constant('WUNDERGROUND_API_KEY', '1aa4dffd1250ef6b')
+/* Accepts a location as a parameter, converts that location into geographic coordinates (latitude and longitude), then returns the lat/long for that location */
+.factory('geocodeLocation', ['$http', 'GOOGLE_GEOCODE_PREFIX', 'GOOGLE_API_KEY', function($http, GOOGLE_GEOCODE_PREFIX, GOOGLE_API_KEY) {
+    return function(location) {
+        var params = {
+            key: GOOGLE_API_KEY,
+            address: location
+        };
+        return $http({
+            method: 'GET',
+            url: GOOGLE_GEOCODE_PREFIX,
+            params: params
+        })
+        .then(function(response) {
+            console.log(response.data.results[0].geometry.location);
+        });
+    };
+}])
+/* Accepts a location (latitude/longitude) as a parameter and returns the current weather conditions at that location */
+.factory('getWeatherConditions', ['$http', 'WUNDERGROUND_PREFIX', 'WUNDERGROUND_API_KEY', function($http, WUNDERGROUND_PREFIX, WUNDERGROUND_API_KEY) {
+    return function(city, state) {
+        return $http({
+            method: 'GET',
+            url: WUNDERGROUND_PREFIX + WUNDERGROUND_API_KEY + '/conditions/q/37.776289,-122.395234' + '.json'
+        })
+        .then(function(response) {
+            // console.log(response.data.current_observation);
+            return response.data;
+        });
+    };
+}]);
