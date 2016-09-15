f491b38f1107e3e0e28fc86e63e504bee8d491dc
Taylor Welling <tw3080@gmail.com>
2016-09-15 16:05:39 -0700

    set scope variables to values taken from weatherAppService for binding to templates; added a 'submit' function to the scope which uses weatherAppService's 'submit' function to update scope variables with new values each time a search is made

--- app/10-day-forecast/10-day.js
+++ app/10-day-forecast/10-day.js
@@ -1,5 +1,33 @@
 viewsModule.config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/10-day', {
-        templateUrl: '10-day-forecast/10-day.html'
+        templateUrl: '10-day-forecast/10-day.html',
+        controller: 'TenDayCtrl'
     });
 }]);
+
+viewsModule.controller('TenDayCtrl', function($scope, $rootScope, weatherAppService, getWeatherConditions) {
+    /* TODO */
+    /*
+    weatherAppService.forecast = 'tenDay';
+    console.log(weatherAppService.forecast);
+    */
+    /*      */
+    $rootScope.weatherClass = 'sunny';
+    // Set scope variables for binding to template
+    $scope.address = weatherAppService.address;
+    $scope.location = weatherAppService.location;
+    $scope.tenDay = weatherAppService.tenDay.data;
+    console.log($scope.tenDay);
+
+    $scope.submit = function(address) {
+        // weatherAppService.forecast = 'tenDay';
+
+        weatherAppService.submit(address, function(location) {
+            $scope.address = location;
+            $scope.location = weatherAppService.location;
+            // console.log($scope.location);
+            $scope.tenDay = weatherAppService.tenDay.data;
+            console.log($scope.tenDay);
+        });
+    };
+});
