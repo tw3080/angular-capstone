473375edc49c93ce3c780158fc72e2b7cbb19c27
Taylor Welling <tw3080@gmail.com>
2016-09-15 16:07:17 -0700

    set scope variables to values taken from weatherAppService for binding to templates; added a 'submit' function to the scope which uses weatherAppService's 'submit' function to update scope variables with new values each time a search is made

--- app/home/home.js
+++ app/home/home.js
@@ -5,13 +5,20 @@ viewsModule.config(['$routeProvider', function($routeProvider) {
     });
 }]);
 
-viewsModule.controller('CurrentWeatherCtrl', function($scope, $rootScope, weatherAppService) {
+viewsModule.controller('CurrentWeatherCtrl', function($scope, $rootScope, weatherAppService, getWeatherConditions) {
+    /* TODO */
+    // weatherAppService.forecast = 'current';
+    // console.log(weatherAppService.forecast);
+    /*      */
+    // Set scope variables for binding to template
     $rootScope.weatherClass = 'rainy';
     $scope.address = weatherAppService.address;
     $scope.location = weatherAppService.location;
     $scope.currentWeather = weatherAppService.currentWeather;
 
     $scope.submit = function(address) {
+        // weatherAppService.forecast = 'current';
+
         weatherAppService.submit(address, function(location) {
             $scope.address = location;
             $scope.location = weatherAppService.location;
@@ -19,27 +26,4 @@ viewsModule.controller('CurrentWeatherCtrl', function($scope, $rootScope, weathe
             $scope.currentWeather = weatherAppService.currentWeather;
         });
     };
-    /*
-    // TODO: Is this the best way to do this? Using $scope doesn't work...
-    $rootScope.address = null;
-    */
-
-    // $rootScope.submit = function(address) {
-    /*
-    $scope.submit = function(address) {
-        $scope.address = address;
-        console.log($scope.address);
-        geocodeLocation($scope.address).then(function(response) {
-            $scope.lat = response.lat;
-            $scope.lng = response.lng;
-            return getWeatherConditions($scope.lat, $scope.lng);
-        }).then(function(response) {
-            console.log(response);
-            // TODO: only rootScope stays when routes change?
-            $rootScope.locationName = response.display_location.city;
-            $rootScope.locationName += ', ';
-            $rootScope.locationName += response.display_location.state;
-        });
-    };
-    */
 });
