3082a3b08084b5e72618035d63e3492c1a9d01ab
Taylor Welling <tw3080@gmail.com>
2016-11-11 15:45:04 -0800

    Play/pause on-click functionality

--- app/audio-control.js
+++ app/audio-control.js
@@ -1,6 +1,5 @@
 weatherApp.directive('audioControl', function() {
     return {
-        // TODO: getting 'undefined' for scope here?
         restrict: 'E',
         templateUrl: 'audio-control.html',
         scope: {
@@ -8,14 +7,28 @@ weatherApp.directive('audioControl', function() {
         },
         link: function(scope, element, attrs, weatherAppService) {
             scope.$watch(function() { return scope.audioFile; }, function() {
-                scope.audioOgg = 'components/audio/' + scope.audioFile + '.ogg';
-                scope.audioMp3 = 'components/audio/' + scope.audioFile + '.mp3';
-                document.getElementById('weather-audio').load().play();
-                // console.log(scope.audioFile);
+                scope.audioOgg = 'assets/audio/' + scope.audioFile + '.ogg';
+                scope.audioMp3 = 'assets/audio/' + scope.audioFile + '.mp3';
+                // TODO: The line below seems unnecessary?
+                // document.getElementById('weather-audio').load().play();
             });
-            // console.log(scope.audioFile);
-            scope.audioOgg = 'components/audio/' + scope.audioFile + '.ogg';
-            scope.audioMp3 = 'components/audio/' + scope.audioFile + '.mp3';
+            scope.audioOgg = 'assets/audio/' + scope.audioFile + '.ogg';
+            scope.audioMp3 = 'assets/audio/' + scope.audioFile + '.mp3';
+
+            scope.paused = false;
+
+            scope.toggleAudio = function() {
+                var audio = document.getElementById('weather-audio');
+                var audioIcon = document.getElementById('audio-icon');
+
+                if (audio.paused) {
+                    scope.paused = false;
+                    audio.play();
+                } else {
+                    scope.paused = true;
+                    audio.pause();
+                }
+            };
         }
     };
 });
