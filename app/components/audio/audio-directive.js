635b7f279268c541652ceb0ab7b27e359f007da8
Taylor Welling <tw3080@gmail.com>
2016-11-18 13:35:57 -0800

    Fixed a bug which was causing the console to throw an error about the audio element not existing before a location was set

--- app/components/audio/audio-directive.js
+++ app/components/audio/audio-directive.js
@@ -6,16 +6,19 @@ weatherApp.directive('audioControl', function() {
             audioFile: '=audioFile'
         },
         link: function(scope, element, attrs, weatherAppService) {
+            // Watch for changes to scope.audioFile (this changes based on the weather conditions) and update the audio file which plays accordingly
             scope.$watch(function() { return scope.audioFile; }, function() {
                 scope.audioOgg = 'assets/audio/' + scope.audioFile + '.ogg';
                 scope.audioMp3 = 'assets/audio/' + scope.audioFile + '.mp3';
-                // TODO: Get an error here if no location is set because of the ng-if in audio-control
-                document.getElementById('weather-audio').load();
-                document.getElementById('weather-audio').play();
+                // Only play the audio if the 'weather-audio' element exists; this prevents the console from throwing errors about the 'weather-audio' element not being defined before a location has been set
+                if (document.getElementById('weather-audio')) {
+                    document.getElementById('weather-audio').load();
+                    document.getElementById('weather-audio').play();
+                }
             });
+            // Set the audio file according to the value of scope.audioFile
             scope.audioOgg = 'assets/audio/' + scope.audioFile + '.ogg';
             scope.audioMp3 = 'assets/audio/' + scope.audioFile + '.mp3';
-
             scope.paused = false;
 
             /* Play/pause audio when audio icon is clicked */
