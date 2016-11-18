f65079ee8f51fb0273f69a1fdce371f3e93e1a4f
Taylor Welling <tw3080@gmail.com>
2016-11-18 13:25:13 -0800

    Injected 'ngAnimate' as a dependency

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
-var weatherApp = angular.module('weatherApp', ['weatherAppViews', 'ngRoute', 'LocalStorageModule'])
+var weatherApp = angular.module('weatherApp', ['weatherAppViews', 'ngRoute', 'ngAnimate', 'LocalStorageModule'])
     .config(function($routeProvider) {
         /* If the user tries to go to any route other than one which is already defined, redirect them to the home page */
         $routeProvider.otherwise({
