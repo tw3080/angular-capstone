39bf0b05507616d66440c423cc4e3429725ed253
Taylor Welling <tw3080@gmail.com>
2016-11-20 22:39:49 -0800

    Removed unnecessary console.logs and comments

--- app/components/saved-cities/saved-cities-controller.js
+++ app/components/saved-cities/saved-cities-controller.js
@@ -4,11 +4,13 @@ viewsModule.config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/saved-cities', {
         // templateUrl: 'components/saved-cities/saved-cities.html',
         template: `<ng-include src="'./components/nav/nav.html'"></ng-include>
-        <h1>Saved Cities</h1>
-        <p>
-            Type in a city name and click 'add' to save it for quick access
-        </p>
-        <saved-cities submit="submit"></saved-cities>`,
+        <div class="saved-cities-container">
+            <h1>Saved Cities</h1>
+            <p class="unimportant-text">
+                Save cities for quick viewing
+            </p>
+        <saved-cities submit="submit"></saved-cities>
+        </div>`,
         controller: 'SavedCitiesCtrl',
         controllerAs: 'savedCities'
     });
@@ -16,14 +18,6 @@ viewsModule.config(['$routeProvider', function($routeProvider) {
 
 viewsModule.controller('SavedCitiesCtrl', function($scope, $rootScope, weatherAppService, getWeatherConditions) {
     // Set scope variables for binding to template
-    // Location
-    /*
-    $scope.address = weatherAppService.address;
-    $scope.location = weatherAppService.location;
-    // Weather forecasts
-    $scope.currentWeather = weatherAppService.currentWeather;
-    $scope.tenDay = weatherAppService.tenDay.data;
-    */
     // Audio/css
     $scope.audioFile = weatherAppService.weatherSounds;
     $rootScope.weatherClass = weatherAppService.weatherClass;
@@ -40,21 +34,11 @@ viewsModule.controller('SavedCitiesCtrl', function($scope, $rootScope, weatherAp
         // Audio/css
         $scope.audioFile = weatherAppService.weatherSounds;
         $rootScope.weatherClass = weatherAppService.weatherClass;
-        // console.log('Show weather after submit: ', $scope.showWeather);
-        console.log('audioFile after submit:', $scope.audioFile);
-        console.log('weatherClass after submit: ', $rootScope.weatherClass);
     };
-
-    // TODO: Comment this out if there are issues
-    /*
-    weatherAppService.initializeGeolocation($scope.weatherCallback);
-    */
 
     /* Uses the submit function from weatherAppService to geocode the address input by the user and gets the current weather/10-day forecast */
     $scope.submit = function(address) {
         weatherAppService.submit(address, $scope.weatherCallback);
         $scope.showWeather = true;
-        console.log('Show weather after submit: ', $scope.showWeather);
-        // console.log('weatherClass after submit: ' + $rootScope.weatherClass);
     };
 });
