bb5f3d0efa69c51bf6bb5de7d6ec867f68971704
Taylor Welling <tw3080@gmail.com>
2016-11-20 22:41:19 -0800

    Fixed error handling for duplicate cities (an error message is now shown to the user); fixed an issue with the 'please enter a location' error message not always showing when it should

--- app/components/saved-cities/saved-cities-directive.js
+++ app/components/saved-cities/saved-cities-directive.js
@@ -8,10 +8,14 @@ viewsModule.directive('savedCities', function() {
             submit: '=submit'
         },
         link: function(scope, element, attrs) {
-            // TODO: Feel like I'm repeating myself too much by having to call getCityList 3 times?
+            scope.city = '';
+            scope.formInvalid = false; // If set to true, an error message asking the user to enter a location will display
+            scope.duplicateCity = false;
 
-            // Gets the list of saved cities
+            /* Gets the list of saved cities */
             scope.getCityList = function() {
+                scope.duplicateCity = false;
+
                 var cityList = [];
                 var data = localStorage.getItem('city');
 
@@ -21,87 +25,48 @@ viewsModule.directive('savedCities', function() {
                 return cityList;
             };
 
-            // Add a city to the saved cities list (which is stored in localstorage)
+            scope.savedCities = scope.getCityList(); // Get the list of cities on page load
+
+            /* Add a city to the saved cities list (which is stored in localstorage) */
             scope.addCity = function() {
                 var city = scope.city; // scope.city is bound to the city input field
                 var cityList = scope.getCityList(); // Get the list of cities
 
+                if (cityList)
+
                 for (var i = 0; i < cityList.length; i++) {
-                    // If the city has already been added, don't add it again, and notify the user
-                    if (cityList[i].name == city) {
-                        // TODO: Display error message here
-                        console.log('duplicate city');
+                    /* If the city has already been added, don't add it again, and notify the user */
+                    console.log(city);
+                    if (cityList[i].name.toUpperCase() == city.toUpperCase()) { // Needs to be case insensitive
+                        scope.duplicateCity = true;
+                        scope.city = '';
                         return;
                     }
                 }
 
-                // Otherwise, if the city hasn't been added, add it to the list
-                cityList.push({
-                    name: city
-                });
-                localStorage.setItem('city', JSON.stringify(cityList));
-                console.log(cityList);
-                // scope.$apply();
-                scope.savedCities = scope.getCityList(); // Update the list of cities
-                scope.city = ''; // Clear city input field
+                /* Otherwise, if the form is valid and if the city hasn't been added, add the city to the list */
+                if (scope.savedCitiesForm.$valid) {
+                    cityList.push({ // Add the city to the cityList array
+                        name: city
+                    });
+                    localStorage.setItem('city', JSON.stringify(cityList)); // Add the city to local storage
+                    scope.savedCities = scope.getCityList(); // Update and store the list of cities
+                    scope.city = ''; // Clear city input field
+                    scope.formInvalid = false; // Don't display the error message
+                    /* Else if the form is invalid and the length of the city list hasn't changed, show the error message */
+                } else if (!scope.savedCitiesForm.$valid && (cityList.length == scope.savedCities.length)) {
+                    scope.formInvalid = true; // Display the error message
+                }
             };
 
-            // Remove a city from the saved cities list
+            /* Remove a city from the saved cities list */
             scope.removeCity = function(city) {
-                console.log('removing city');
-                console.log(city);
                 var cityList = scope.getCityList();
 
-                cityList.splice(city, 1);
-                localStorage.setItem('city', JSON.stringify(cityList));
-                console.log(cityList);
-                scope.savedCities = scope.getCityList(); // Update the list of cities
-                /*
-                for (var i = 0; i < cityList.length; i++) {
-                    if (cityList[i] == city) {
-                        cityList.splice(i, 1);
-                        localStorage.setItem('city', JSON.stringify(cityList));
-                    }
-                }
-                */
+                cityList.splice(city, 1); // Remove the city from the cityList array
+                localStorage.setItem('city', JSON.stringify(cityList)); // Remove the city from local storage
+                scope.savedCities = scope.getCityList(); // Update and store the list of cities
             };
-
-            scope.savedCities = scope.getCityList(); // Get the list of cities on page load
-            console.log(scope.savedCities);
-
-            // Add a city to the 'savied cities' list; the list is stored in localstorage
-            /*
-            scope.addCity = function() {
-                console.log('city added');
-                scope.city = scope.city;
-
-                if (localStorage.cities) {
-                    cities = JSON.parse(localStorage.cities);
-                } else {
-                    cities = [];
-                }
-
-                cities.push(scope.city);
-                localStorage.cities = JSON.stringify(cities);
-                cities = JSON.parse(localStorage.cities);
-
-                console.log(cities);
-            };
-            */
-
-            // Remove a saved city
-            /*
-            scope.deleteCity = function(city) {
-                cities = JSON.parse(localStorage.cities);
-
-                for (var i = 0; i < cities.length; i++) {
-                    if (cities[i] == city) {
-                        cities.splice(i, 1);
-                        localStorage.cities = JSON.stringify(cities);
-                    }
-                }
-            };
-            */
         }
     };
 });
